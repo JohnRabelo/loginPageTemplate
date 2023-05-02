40f9e226e0ae952c044589f7057486706f174f83
johnr21 <johnrabelo.contact@gmail.com>
2023-05-02 18:14:16 -0300

    backdrop

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Modal from './components/Modal';
+import Backdrop from './components/Backdrop';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          A new login page template is coming soon
-        </p>
-      </header>
+    <div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            A new login page template is coming soon
+          </p>
+        </header>
+      </div>
+      <Modal />
+      <Backdrop />
     </div>
   );
 }
---
+++ src/components/Modal.js
@@ -0,0 +1,12 @@
+import modalcl from './Modal.module.css'
+
+function Modal(){
+    return <div className='modal'>
+        <p>By proceeding, you agree with our cookies policy</p>
+        <button>Proceed</button>
+        <button>Leave</button>
+    </div>
+}
+
+
+export default Modal;
