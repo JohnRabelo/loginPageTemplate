76aed81d49bd59b20b447132e83c20f31caeb7b3
johnr21 <johnrabelo.contact@gmail.com>
2023-05-18 18:24:45 -0300

    v1

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -7,7 +7,7 @@ function Modal(props){
 
     return <div className={modalcl.modal}>
         <div className={modalcl.modalBox}>
-            <p>By proceeding, you agree with our <a href='http://dossombreros.co.uk/privacy-policy/' target='_blank' rel="noreferrer">privacy policy</a>.</p>
+            <p>By proceeding, you agree to our <a href='http://dossombreros.co.uk/privacy-policy/' target='_blank' rel="noreferrer">privacy policy</a>.</p>
             <button onClick={props.onCancel}>Proceed</button>
             <button onClick={props.onCancel}>Reject All</button>
         </div>
